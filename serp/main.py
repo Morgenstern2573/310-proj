ae2bbb63aafd59d64c35554a998a584704b065c0
Bosun Akinyemi <azurnwa.p@gmail.com>
2022-11-02 08:53:49 +0100

    filtered search results to just urls

--- serp/main.py
+++ serp/main.py
@@ -4,7 +4,7 @@ from urllib.parse import quote_plus
 # outline main function
 def main():
     # accept keyword from the user
-    query = quote_plus(input("Enter space-separated keywords").strip())
+    query = quote_plus(input("Enter space-separated keywords: ").strip())
     # use keyword to make API call with requests
     path = f"https://www.googleapis.com/customsearch/v1?key=AIzaSyC9-yThffMzzsDYcQN7aZZUyVtre8bySsU&cx=93b7cb032f56c40e2&q={query}"
     resp = requests.get(path)
@@ -16,8 +16,10 @@ def main():
         return
     # parse the JSON the api returns
     data = resp.json()
-    print(data) 
+    search_results = [x["link"] for x in data["items"]]
+    print(search_results) 
     # store the urls in dictionary
+    summary_data = dict()
     # use beautiful soup & requests to 
     # get website content and store in dictionary
     # make scraping process concurrent
